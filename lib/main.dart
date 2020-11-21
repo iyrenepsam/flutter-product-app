63230ff3ade3d93caf0acaf876baa7f819773c3c
iyrenepsam <irenepsam@gmail.com>
2020-11-21 19:41:36 +0530

    botton event

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,32 @@ import 'package:flutter/material.dart';
 void main()=>runApp(ProductApp());
 
 
-class ProductApp extends StatelessWidget {
+class ProductApp extends StatefulWidget {
+  @override
+  _ProductAppState createState() => _ProductAppState();
+}
+
+class _ProductAppState extends State<ProductApp> {
+
+  TextEditingController getProName=TextEditingController();
+  TextEditingController getProCode=TextEditingController();
+  TextEditingController getMName=TextEditingController();
+  TextEditingController getMDate=TextEditingController();
+  TextEditingController getExpiryDate=TextEditingController();
+  TextEditingController getProDesc=TextEditingController();
+  TextEditingController getMRP=TextEditingController();
+  TextEditingController getPrice=TextEditingController();
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.brown,
-          title: Text("ProductApp"),
+          backgroundColor:Colors.white,
+          title: Text("PRODUCT APP"),
         ),
         body: Container(
           padding: EdgeInsets.all(25.5),
@@ -24,20 +41,23 @@ class ProductApp extends StatelessWidget {
             child: Column(
               children: [
                 TextField(
+                  controller: getProName,
+                  decoration: InputDecoration(
+                    hintText: "PRODUCT NAME",
+                    border: OutlineInputBorder()
+                  ),
+                ),
+                TextField(
+                  controller: getProCode,
                   decoration: InputDecoration(
                     hintText: "PRODUCT CODE",
                     border: OutlineInputBorder()
                   ),
                 ),
                 SizedBox(height: 15.5),
+
                 TextField(
-                  decoration: InputDecoration(
-                    hintText: "PRODUCT NAME",
-                    border: OutlineInputBorder()
-                  ),
-                ),
-                SizedBox(height: 15.5),
-                TextField(
+                  controller: getMName,
                   decoration: InputDecoration(
                     hintText: "MANUFACTURER NAME",
                     border: OutlineInputBorder()
@@ -45,6 +65,7 @@ class ProductApp extends StatelessWidget {
                 ),
                 SizedBox(height: 15.5),
                 TextField(
+                  controller: getMDate,
                   decoration: InputDecoration(
                     hintText: "MANUFACTURING DATE",
                     border: OutlineInputBorder()
@@ -52,6 +73,7 @@ class ProductApp extends StatelessWidget {
                 ),
                 SizedBox(height: 15.5),
                 TextField(
+                  controller: getExpiryDate,
                   decoration: InputDecoration(
                     hintText: "EXPIRY DATE",
                     border: OutlineInputBorder()
@@ -59,6 +81,7 @@ class ProductApp extends StatelessWidget {
                 ),
                 SizedBox(height: 15.5),
                 TextField(
+                  controller: getProDesc,
                   decoration: InputDecoration(
                     hintText: "PRODUCT DESCRIPTION",
                     border: OutlineInputBorder()
@@ -66,6 +89,7 @@ class ProductApp extends StatelessWidget {
                 ),
                 SizedBox(height: 15.5),
                 TextField(
+                  controller: getMRP,
                   decoration: InputDecoration(
                     hintText: "MRP",
                     border: OutlineInputBorder()
@@ -73,6 +97,7 @@ class ProductApp extends StatelessWidget {
                 ),
                 SizedBox(height: 15.5),
                 TextField(
+                  controller: getPrice,
                   decoration:InputDecoration(
                     hintText: "PRICE",
                     border: OutlineInputBorder()
@@ -80,7 +105,31 @@ class ProductApp extends StatelessWidget {
                 ),
                 SizedBox(height: 15.5),
 
-                RaisedButton(onPressed: (){
+                RaisedButton(
+
+                  onPressed: (){
+
+                    String getPName=getProName.text;
+                    String getPcode=getProCode.text;
+                    String getMN=getMName.text;
+                    String getMD=getMDate.text;
+                    String getED=getExpiryDate.text;
+                    String getPDsc=getProDesc.text;
+                    String getMrp=getMRP.text;
+                    String getCost=getPrice.text;
+
+
+                    print(getPName);
+                    print(getPcode);
+                    print(getMN);
+                    print(getMD);
+                    print(getED);
+                    print(getPDsc);
+                    print(getMrp);
+                    print(getCost);
+
+
+
                   
                 },
                   color: Colors.blue,
